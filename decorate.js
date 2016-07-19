1d92713d81027618a9d429545e2e8f1590120602
bishopjustice <luke@lyft.com>
2016-07-19 15:14:16 -0700

    Added radio buttons and a start button

--- decorate.js
+++ decorate.js
@@ -1,15 +1,98 @@
-var base = "http://thecatapi.com/api/images/get?size=med&type=jpg,png&category=";
-var key = ['hats', 'space', 'funny', 'sunglasses', 'boxes', 'caturday', 'ties', 'dream', 'kittens', 'sinks'];
+// 
 
-var choices = {
-    all: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&",
-    hats: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=hats&",
-    boxes: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=boxes&",
-    ties: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=ties&",
-    sunglasses: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=sunglass"
+function show_buttons() {
+    document.getElementById('radios').style.display = "block";
+    document.getElementById('start').style.display = "none";
 }
 
 
-setInterval(function(choice = "ties") {
-    document.getElementById('imgDisplay').src = "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&" + new Date().getTime();
-}, 3000);
+function selector(choice){
+    if (choice == 'all'){
+        document.getElementById('imgDisplay').src = "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&" + new Date().getTime()
+        console.log(document.getElementById('imgDisplay').src)
+}
+    else{
+        document.getElementById('imgDisplay').src = "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category="+ choice + "&" + new Date().getTime()
+        console.log(document.getElementById('imgDisplay').src)}
+}
+
+
+var myVar;
+
+function startFunction(choice){
+    clearInterval(myVar);
+    myVar = setInterval(selector, 3000, choice);
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
+
+
+
+// var choices = {
+//     all: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&",
+//     hats: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=hats&",
+//     boxes: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=boxes&",
+//     ties: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=ties&",
+//     sunglasses: "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&category=sunglass"
+// }
+
+// function cat_pics() {
+//     document.getElementById('imgDisplay').src = "http://thecatapi.com/api/images/get?size=med&type=jpg,png&api_key=MTAzMzIw&" + new Date().getTime();
+//     setInterval(cat_pics(), 3000);
+// }
+
+// function timer(choice){
+//             clearInterval(myVar);
+//             var myVar = setInterval(function(){ selector(choice) }, 2000);
+//             return myVar
+//     }
+
+
+
+
+
+// function selector(){
+//     if (document.getElementById('all').checked){
+//         console.log("it works")
+//     }
+//     else if (document.getElementById('hats').checked){
+//         console.log("it works")
+//     }
+// }
+
+
+// function rotate() {
+//     if (document.getElementById('all').checked) {
+//         setInterval(function() {
+//             document.getElementById('imgDisplay').src = choices.all + new Date().getTime();
+//             console.log(document.getElementById('imgDisplay').src)
+//         }, 3000);
+//     } else if (document.getElementById('hats').checked) {
+//         setInterval(function() {
+//             document.getElementById('imgDisplay').src = choices.hats + new Date().getTime();
+//             console.log(document.getElementById('imgDisplay').src)
+//         }, 3000);
+//     } else if (document.getElementById('ties').checked) {
+//         setInterval(function() {
+//             document.getElementById('imgDisplay').src = choices.ties + new Date().getTime();
+//             console.log(document.getElementById('imgDisplay').src)
+//         }, 3000);
+//     } else if (document.getElementById('sunglasses').checked) {
+//         setInterval(function() {
+//             document.getElementById('imgDisplay').src = choices.sunglasses + new Date().getTime();
+//             console.log(document.getElementById('imgDisplay').src)
+//         }, 3000);
+//     }
+// }
+
+// if(document.getElementById('all').checked) {
+//   rotate()
+// }else if(document.getElementById('hats').checked) {
+//   //Female radio button is checked
+// }else if(document.getElementById('ties').checked) {
+//   //Female radio button is checked
+// }else if(document.getElementById('sunglasses').checked) {
+//   //Female radio button is checked
+// }
